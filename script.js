1f171864789ed2ffa0c747e9aa05928c75b88852
CharaFun <albert777apos@gmail.com>
2025-04-06 00:42:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function changeText() {
     let text = document.getElementById("text");
-    text.innerText = "Текст изменён!";
-    text.style.color = "red";
+    text.innerText = "ТЕКСТ ИЗМЕНИЛСЯ!";
+    text.style.color = "blue";
 }
