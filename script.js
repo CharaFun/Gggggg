0cafec53eede11f826cd66c9e1abbd9e1defcd25
CharaFun <albert777apos@gmail.com>
2025-04-06 01:17:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function changeText() {
-    let text = document.getElementById("text");
-    text.innerText = "ТЕКСТ ИЗМЕНИЛСЯ!";
-    text.style.color = "blue";
+function showAlert() {
+    alert("Секрет: этот сайт сделан с помощью Grok!");
+}
+function updateText() {
+    let input = document.getElementById("input").value;
+    let text = document.getElementById("about-text");
+    if (input) {
+        text.innerText = input;
+        text.style.color = "green";
+    } else {
+        text.innerText = "Введите текст!";
+        text.style.color = "red";
+    }
+}
+function changeEmail() {
+    let email = document.getElementById("email");
+    email.innerText = "newemail@mail.com";
+    email.style.color = "#007BFF";
 }
